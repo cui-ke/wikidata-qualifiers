40df76868764f4366a39dd1d896db96837cca8b7
Gilles Falquet <gilles.falquet@unige.ch>
2025-05-02 02:44:22 +0200

    results and trace to file

--- src/stats/count_stmt.py
+++ src/stats/count_stmt.py
@@ -2,13 +2,15 @@
 
 """
 input: a wikidata json dump (in std input)
+output: results in count_stmt.out
 
-
-usage: bzip2 -dk wikidata-dump.json.bz2 | python path-to-count_stmt.py
+usage: bzip2 -cdk wikidata-dump.json.bz2 | python <path-to-count_stmt.py>
 """
 
 import sys
 import json
+
+fo = open('count_stmt.out','w')
 
 stmt_c = 0
 stmt_with_q_c = 0
@@ -20,9 +22,9 @@ for line in sys.stdin:
         while ls[-1] != '}' : ls = ls[:-1]
         entity = json.loads(ls)
         entity_c += 1
-        # print(j['id'])
+        # print(entity['id'])
 
-        claims = entity["claims"] if "claims" in j else {}
+        claims = entity["claims"] if "claims" in entity else {}
 
         for p in claims:
             stmt_c += len(claims[p])
@@ -31,9 +33,9 @@ for line in sys.stdin:
                     if len(stmt["qualifiers"]) > 0 :
                         stmt_with_q_c += 1                
                     
-        if entity_c % 10 == 0:
-            print(f'e: {entity_c}, stmt: {stmt_c}, qualified: {stmt_with_q_c}')
+        if entity_c % 1000 == 0:
+            fo.write(f'e: {entity_c:_}, stmt: {stmt_c:_}, qualified: {stmt_with_q_c:_}, {stmt_with_q_c/stmt_c*100:8.4f}\n')
+            fo.flush()
 
-
-
-
+fo.write(f'e: {entity_c}, stmt: {stmt_c}, qualified: {stmt_with_q_c}, {stmt_with_q_c/stmt_c*100}\n')
+fo.close()
